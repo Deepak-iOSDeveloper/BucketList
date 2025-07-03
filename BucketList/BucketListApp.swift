2545cfbce1ab9137542d1971a000c60fa974584a
deepak <deepakjgrt99@gmail.com>
2025-07-03 10:19:09 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by DEEPAK BEHERA on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
